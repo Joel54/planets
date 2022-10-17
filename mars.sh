8c50efe2001cf487639918d52b4d2fa62acf5178
Joel Kidgell <Joel.Kidgell@gmail.com>
2022-10-17 16:15:13 +1300

    Added test line for remote push

--- mars.sh
+++ mars.sh
@@ -3,3 +3,4 @@ echo "Cold and dry, but everything is my favourite colour"
 echo "The two moods may be a problem for Wolfman"
 echo "But the mummy will appreciate the lack of humidity"
 
+echo "adding line to push to remote"
