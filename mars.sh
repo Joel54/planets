ece595ada2314a4293106b63fb168599c8e22af3
Joel Kidgell <Joel.Kidgell@gmail.com>
2022-10-17 15:14:52 +1300

    fixed up the merge error

--- mars.sh
+++ mars.sh
@@ -2,7 +2,3 @@
 echo "Cold and dry, but everything is my favourite colour"
 echo "The two moods may be a problem for Wolfman"
 echo "But the mummy will appreciate the lack of humidity"
-<<<<<<< HEAD
-error is here
-=======
->>>>>>> parent of 8dbfa17 (Added some nonsence words to the script)
